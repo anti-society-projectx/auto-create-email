78abd8159e575ca5575467b44fbf449871c454b5
anti-society-projectx <anti-society-projectx@gmail.com>
2026-04-09 01:39:35 +0300

    feat: добавлена отработка текста при выборе создания в главном меню

--- src/core/console.py
+++ src/core/console.py
@@ -2,6 +2,7 @@ from pyfiglet import figlet_format
 from rich.console import Console
 from rich.align import Align
 from rich.panel import Panel
+from rich.prompt import Prompt
 from rich.table import Table
 from rich.text import Text
 
@@ -36,7 +37,15 @@ class ConsoleUI:
         )
 
     def select_action(self) -> int:
-        select_text = (
-            f"\n{3 * ' '}[bold cyan]Выберете действие: [/bold cyan]"
+        input_text = (
+            f"[bold white]❯ Выберете действие: [/bold white]"
         )
-        return int(self.console.input(select_text))
+        return int(self.console.input(input_text))
+
+    def run_script(self) -> int:
+        input_text = (
+            "[dim]Количество создаваемых почты = количество прокси * количество запросов * 2\n"
+            "Чтобы вернуться в главное меню, укажите 0[/dim]\n"
+            "[bold white]❯ Введите количество запросов: [/bold white]"
+        )
+        return int(self.console.input(input_text))
